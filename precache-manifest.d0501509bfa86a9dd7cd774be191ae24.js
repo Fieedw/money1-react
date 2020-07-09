346ae1844c5749df1af9c13940b490e0a0d78ab8
fieedw <frankding8843@gmail.com>
2020-07-09 18:37:02 +0800

    init

---
+++ precache-manifest.d0501509bfa86a9dd7cd774be191ae24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f07e1df68477c963e85187a49ae292b",
+    "url": "/money1-react/index.html"
+  },
+  {
+    "revision": "c07cb711522d9aca993b",
+    "url": "/money1-react/static/css/main.ec3b4a30.chunk.css"
+  },
+  {
+    "revision": "158023508f2adb08ce7c",
+    "url": "/money1-react/static/js/2.84d300bc.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/money1-react/static/js/2.84d300bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c07cb711522d9aca993b",
+    "url": "/money1-react/static/js/main.c4a25723.chunk.js"
+  },
+  {
+    "revision": "a42e29772a8c14607cf5",
+    "url": "/money1-react/static/js/runtime-main.25eafbce.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/money1-react/precache-manifest.d0501509bfa86a9dd7cd774be191ae24.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/money1-react/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
